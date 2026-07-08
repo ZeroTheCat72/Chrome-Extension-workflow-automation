69897387754503f2630c0aca0571620264a4152a
ZeroTheCat72 <197370694+ZeroTheCat72@users.noreply.github.com>
2026-07-08 15:23:50 +0300

    Correct JSON syntax in manifest.json generation
    Fix JSON formatting in manifest.json creation.

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ cd chromeExtensionWorkspace
 git init
 
 
-echo -e "{\n  \"name\": \"$2\",\n  \"version\": "0.0.1",\n  \"manifest_version\": 3,\n  "action": {\n    "default_popup": "index.html"\n  },\n  "content_scripts": [\n    {\n      "js": ["scripts/content.js"],\n    "matches": [\n      "URL1",\n      "URL2"\n      ]\n    }\n  ]\n}" > manifest.json
+echo -e "{\n  \"name\": \"$2\",\n  \"version\": "0.0.1",\n  \"manifest_version\": 3,\n  "action": {\n    "default_popup": "index.html"\n  },\n  "content_scripts": [\n    {\n      "js": ["scripts/content.js"]\n    "matches": [\n      "URL1",\n      "URL2"\n      ]\n    }\n  ]\n}" > manifest.json
 echo -e "<html>\n  <body>\n    <h1>Hello Extensions</h1>\n  </body>\n</html>" > index.html
 
 mkdir scripts
